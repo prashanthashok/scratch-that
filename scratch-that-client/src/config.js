eb123da276d5be9050dcd1308359b1be394a2434
prashanthashok <prashanth.ashokram@gmail.com>
2019-08-14 23:09:47 -0400

    Adding AWS Amplify config

---
+++ scratch-that-client/src/config.js
@@ -0,0 +1,16 @@
+export default {
+  s3: {
+    REGION: "us-east-2",
+    BUCKET: "park-scratch-app-uploads"
+  },
+  apiGateway: {
+    REGION: "us-east-2",
+    URL: "https://au0mzyvnq3.execute-api.us-east-2.amazonaws.com"
+  },
+  cognito: {
+    REGION: "us-east-2",
+    USER_POOL_ID: "us-east-2_r1suNLGqt",
+    APP_CLIENT_ID: "5psupkgk3rujvvs76aomvr50r",
+    IDENTITY_POOL_ID: "us-east-2:429d7b4b-b9b2-4a00-8ba1-c70cbcdcacb8"
+  }
+};
